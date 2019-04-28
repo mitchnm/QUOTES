1d063e0e383bdfb0750f78329b646e29d67157c6
mitchnm <mitchndungu21@gmail.com>
2019-04-28 23:31:01 +0300

    added dislike button

--- src/app/like/like.component.ts
+++ src/app/like/like.component.ts
@@ -7,13 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LikeComponent implements OnInit {
 numberOfLikes : number = 0;
+numberOfDislikes : number = 0
 
 likeButtonClick() {
 this.numberOfLikes++;
 }
  
 dislikeButtonClick() {
-  this.numberOfLikes--;
+  this.numberOfDislikes++;
 }
   constructor() { }
 
