19d13a1941d049f8eb9e5b1ca5c96ff88aa412d8
mitchnm <mitchndungu21@gmail.com>
2019-04-29 15:16:51 +0300

    changed button

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,10 +10,10 @@ export class QuoteComponent implements OnInit {
 
   quotes = [
     new Quote(1, 'Our greatest glory is not in never falling, but in rising every time we fall.','~ Confucius' ,new Date(2019,3,28)),
-    new Quote(2, 'Everything you’ve ever wanted is on the other side of fear.','~ George Addair',new Date(2019,10,14)),
-    new Quote(3, 'Hardships often prepare ordinary people for an extraordinary destiny.','~ C.S. Lewis',new Date(2019,10,14)),
-    new Quote(4, 'Your true success in life begins only when you make the commitment to become excellent at what you do.','~ Brian Tracy',new Date(2019,10,14)),
-    new Quote(5, 'If you believe it will work out, you’ll see opportunities. If you believe it won’t, you will see obstacles.','~ Wayne Dyer',new Date(2019,10,14)),
+    new Quote(2, 'Everything you’ve ever wanted is on the other side of fear.','~ George Addair',new Date(2019,3,27)),
+    new Quote(3, 'Hardships often prepare ordinary people for an extraordinary destiny.','~ C.S. Lewis',new Date(2019,3,29)),
+    new Quote(4, 'Your true success in life begins only when you make the commitment to become excellent at what you do.','~ Brian Tracy',new Date(2019,3,14)),
+    new Quote(5, 'If you believe it will work out, you’ll see opportunities. If you believe it won’t, you will see obstacles.','~ Wayne Dyer',new Date(2019,3,30)),
     new Quote(6, 'Success means doing the best we can with what we have. Success is the doing, not the getting; in the trying, not the triumph. Success is a personal standard, reaching for the highest that is in us, becoming all that we can be.','~ Zig Ziglar',new Date(2019,10,14)),
     new Quote(7, 'If you set goals and go after them with all the determination you can muster, your gifts will take you places that will amaze you.','~ Les Brown',new Date(2019,10,14)),
     new Quote(8, 'Whatever you hold in your mind on a consistent basis is exactly what you will experience in your life.','~ Tony Robbins',new Date(2019,10,14)),
